5f56d679224cf4b4821facb797cdfdd34ce2e948
Carneiro1908 <tomasclima1406@gmail.com>
2026-06-22 14:29:24 +0100

    Add: roles & starting to develop a new workflow

--- terraform/iam_roles.tf
+++ terraform/iam_roles.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "ec2-ecr-role" {
+resource "aws_iam_role" "ec2_ecr_role" {
   name = "ec2-ecr-readonly-role"
   
   assume_role_policy = jsonencode({
@@ -23,4 +23,27 @@ resource "aws_iam_role_policy_attachment" "ecr_readonly" {
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2-ecr-readonly-profile"
   role = aws_iam_role.ec2-ecr-role.name
+}
+
+resource "aws_iam_role" "ecr_full_access" {
+  name = "github-actions-ecr-full-acsses"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [{
+      Effect = "Allow"
+
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.github.arn
+      }
+
+      Action = "sts:AssumeRoleWithWebIdentity"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_full_access" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
 }
